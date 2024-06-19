9aebe889836a572f96ab345e3eea0cf8a1b4e28d
Herbert Lima <herbert.lima@pm.df.gov.br>
2024-06-19 17:54:31 -0300

    Etapas com base no Banco

--- src/main/java/com/policia/df/bot/app/command/OtpCommand.java
+++ src/main/java/com/policia/df/bot/app/command/OtpCommand.java
@@ -1,10 +1,13 @@
 package com.policia.df.bot.app.command;
 
+import com.policia.df.bot.app.entities.EtapaEntity;
 import com.policia.df.bot.app.entities.SessaoEntity;
+import com.policia.df.bot.app.repository.EtapaRepository;
 import com.policia.df.bot.app.repository.SessaoRepository;
 import com.policia.df.bot.app.service.enuns.EtapaPadraoEnum;
 import com.policia.df.bot.core.service.KeycloakService;
 import com.policia.df.bot.core.v1.dto.DecisaoResposta;
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.representations.idm.UserRepresentation;
@@ -15,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.BiFunction;
+import java.util.regex.Pattern;
 
 @Component("otpCommand")
 @Getter
@@ -26,14 +30,39 @@ public class OtpCommand implements CommandStrategy {
 
     private final KeycloakService keycloakService;
 
-    public OtpCommand(KeycloakService keycloakService, SessaoRepository sessaoRepository) {
-        this.keycloakService = keycloakService;
+    private final EtapaRepository etapaRepository;
 
-        relacaoDeEtapasEFuncoes.put(EtapaPadraoEnum.INIT.getValor(), (texto, sessao) -> resolveDecisao("Por favor, digite o nome de usuário. Ex: fulano.ciclano", "step_otp"));
-        relacaoDeEtapasEFuncoes.put("step_otp", this::keycloak);
+    public OtpCommand(KeycloakService keycloakService, SessaoRepository sessaoRepository, EtapaRepository etapaRepository) {
+
+        this.keycloakService = keycloakService;
         this.sessaoRepository = sessaoRepository;
+        this.etapaRepository = etapaRepository;
+
+//        relacaoDeEtapasEFuncoes.put(EtapaPadraoEnum.INIT.getValor(), (texto, sessao) -> resolveDecisao("Por favor, digite o nome de usuário. Ex: fulano.ciclano.", "step_otp"));
+//        relacaoDeEtapasEFuncoes.put("step_otp", this::keycloak);
+
     }
 
+    @PostConstruct
+    private void init() {
+
+        List<EtapaEntity> listaEtapas = etapaRepository.findAllByComandoEtapa("otpCommand");
+
+        listaEtapas.forEach(e -> {
+            if("texto".equalsIgnoreCase(e.getTipoEtapa())) {
+
+                relacaoDeEtapasEFuncoes.put(e.getNomeEtapa(), (texto, sessao) -> resolveDecisao(e.getMensagemEtapa(), e.getProximaEtapa()));
+
+            } else {
+
+                relacaoDeEtapasEFuncoes.put(e.getNomeEtapa(), this::keycloak);
+
+            }
+        });
+
+//        relacaoDeEtapasEFuncoes.put(EtapaPadraoEnum.INIT.getValor(), (texto, sessao) -> resolveDecisao("Por favor, digite o nome de usuário. Ex: fulano.ciclano.", "step_otp"));
+//        relacaoDeEtapasEFuncoes.put("step_otp", this::keycloak);
+    }
 
     @Override
     public Map<String, BiFunction<String, SessaoEntity, DecisaoResposta>> getFuncaoEtapas() {
@@ -42,6 +71,11 @@ public class OtpCommand implements CommandStrategy {
 
 
     public DecisaoResposta keycloak(String texto, SessaoEntity sessao) {
+
+        if(!isUsuarioDigitadoCorretamente(texto)) {
+
+            return resolveDecisao("Usuário digitado incorretamente. O usuário possui o seguinte padrão: ''fulano.ciclano''.", "step_otp");
+        }
 
         Optional<List<UserRepresentation>> listUsuarios = keycloakService.pesquisarUsuario(texto.toLowerCase());
 
@@ -62,13 +96,18 @@ public class OtpCommand implements CommandStrategy {
                 log.error("Erro na busca do usuário no keycloak. Erro ", e);
                 return resolveDecisao("Erro ao deletar usuário. Informe novamente.", "step_otp");
             }
-        }else{
+        } else {
 
             return resolveDecisao("Usuário não encontrado. Informe novamente.", "step_otp");
 
         }
+    }
 
+    private Boolean isUsuarioDigitadoCorretamente(String usuario) {
 
+        Pattern pattern = Pattern.compile("\\w+\\.\\w+", Pattern.CASE_INSENSITIVE);
+
+        return pattern.matcher(usuario).find();
     }
 
 }
--- src/main/java/com/policia/df/bot/app/repository/EtapaRepository.java
+++ src/main/java/com/policia/df/bot/app/repository/EtapaRepository.java
@@ -3,7 +3,9 @@ package com.policia.df.bot.app.repository;
 import com.policia.df.bot.app.entities.EtapaEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface EtapaRepository extends MongoRepository<EtapaEntity, String> {
 
-
+    public List<EtapaEntity> findAllByComandoEtapa(String comandoEtapa);
 }
