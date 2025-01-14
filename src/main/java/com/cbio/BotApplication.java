63eabd94a1d90379e68d0c199dd729dbe2c635ef
franklim.paulino <franklim.ti@gmail.com>
2025-01-14 14:37:12 -0300

    [fix] Teste resttemplate

--- src/main/java/com/cbio/BotApplication.java
+++ src/main/java/com/cbio/BotApplication.java
@@ -3,10 +3,8 @@ package com.cbio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,12 +12,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableAsync
 public class BotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BotApplication.class, args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 }
