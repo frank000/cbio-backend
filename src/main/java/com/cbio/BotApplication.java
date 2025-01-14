5041227711ab7168e549fabfbd45629b6d4e056f
franklim.paulino <franklim.ti@gmail.com>
2025-01-14 16:59:06 -0300

    [fix] Correçao spring boot ia

--- src/main/java/com/cbio/BotApplication.java
+++ src/main/java/com/cbio/BotApplication.java
@@ -3,8 +3,10 @@ package com.cbio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,8 +14,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 public class BotApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BotApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BotApplication.class, args);
+	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
