7686a08d063bfcc0accaf2d1f2fd73f1d203310d
franklim.paulino <franklim.ti@gmail.com>
2025-01-23 15:31:15 -0300

    [fix] Melhoria no log para rastreamento v2

--- src/main/java/com/cbio/rasa/action/RagQueryAction.java
+++ src/main/java/com/cbio/rasa/action/RagQueryAction.java
@@ -111,7 +111,7 @@ public class RagQueryAction implements Action {
 
                     EmbeddingSearchRequest searchRequest = EmbeddingSearchRequest.builder()
                             .maxResults(1)
-                            .minScore(0.72)
+                            .minScore(0.6)
                             .queryEmbedding(queryEmbedding)
                             .build();
 
