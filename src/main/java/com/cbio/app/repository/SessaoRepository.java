06e77f8b8ff88f14e3b05938959f7fa48310314b
franklim.paulino <franklim.ti@gmail.com>
2025-03-02 11:20:20 -0300

    [FIX] log para expiration time google credential

--- src/main/java/com/cbio/app/repository/SessaoRepository.java
+++ src/main/java/com/cbio/app/repository/SessaoRepository.java
@@ -2,6 +2,8 @@ package com.cbio.app.repository;
 
 import com.cbio.app.entities.SessaoEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,8 +11,14 @@ public interface SessaoRepository extends MongoRepository<SessaoEntity, String> 
 
     Optional<SessaoEntity> findByAtivoAndIdentificadorUsuarioAndCanalNomeAndCanalIdCanal(Boolean ativo, Long IdentificadorUsuario, String canal, String idCanal);
 
-    Optional<SessaoEntity> findByAtivoAndIdentificadorUsuarioAndCanalIdCanal(Boolean ativo, Long IdentificadorUsuario, String idCanal);
+//    Optional<SessaoEntity> findByAtivoAndIdentificadorUsuarioAndCanalIdCanal(Boolean ativo, Long IdentificadorUsuario, String idCanal);
 
+    @Query(value = "{ 'ativo': ?0, 'identificadorUsuario': ?1, 'canal.idCanal': ?2 }")
+    Optional<SessaoEntity> findByAtivoAndIdentificadorUsuarioAndCanalIdCanal(
+            @Param("ativo") Boolean ativo,
+            @Param("identificadorUsuario") Long identificadorUsuario,
+            @Param("idCanal") String idCanal
+    );
 
     Optional<SessaoEntity> findByAtivoAndIdentificadorUsuarioAndCanalNomeAndLastChannelChatChannelUuid(Boolean ativo, Long IdentificadorUsuario, String canal, String channelUuid);
 
